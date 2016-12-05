7c203d35abe300c027cc5c0e9e2167ce452e8512
Jonas Oberle <jonas.oberle@uni-ulm.de>
2016-12-05 13:40:12 +0100

    parallelisation for random init started

--- session13/hpc/tests/random_init4.cpp
+++ session13/hpc/tests/random_init4.cpp
@@ -7,25 +7,34 @@
 template<typename MA>
 typename std::enable_if<hpc::matvec::IsRealGeMatrix<MA>::value, void>::type
 randomInit(MA& A) {
-   using ElementType = typename MA::ElementType;
-   using Index = typename MA::Index;
+    using ElementType = typename MA::ElementType;
+    using Index = typename MA::Index;
 
-   std::random_device random;
-   std::mt19937 mt(random());
-   std::uniform_real_distribution<ElementType> uniform(-100,100);
-   
-   hpc::matvec::apply(A, [&](ElementType& val, Index i, Index j) -> void {
-      val = uniform(mt);
-   });
+    std::random_device random;
+    std::mt19937 mt(random());
+    std::uniform_real_distribution<ElementType> uniform(-100,100);
+
+    hpc::matvec::apply(A, [&](ElementType& val, Index i, Index j) -> void {
+        val = uniform(mt);
+    });
 }
 
 int main() {
-   using namespace hpc::matvec;
-   GeMatrix<double> A(1000, 1000);
+    using namespace hpc::matvec;
+    GeMatrix<double> A(1000, 1000);
 
-   randomInit(A);
+    std::size_t jobSize = A.numCols/2;
 
-   /* print a small block of each of the initialized matrices */
-   auto A1 = A(500, 500, 5, 5);
-   print(A1, "A1");
+    auto A_1 = A(A.numRows, jobSize, 0, 0);
+    auto A_2 = A(A.numRows, jobSize, 0, jobSize);
+
+    std::thread t1([&](){randomInit(A_1);});
+    std::thread t2([&](){randomInit(A_2);});
+
+    t1.join();
+    t2.join();
+
+    /* print a small block of each of the initialized matrices */
+    auto A1 = A(500, 500, 5, 5);
+    print(A1, "A1");
 }
