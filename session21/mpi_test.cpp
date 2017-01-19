3af6588714ccd53a661e90bebfc3c40f16616c0c
Jonas Oberle <jonas.oberle@uni-ulm.de>
2017-01-19 20:52:24 +0100

    session 21 started

---
+++ session21/mpi_test.cpp
@@ -0,0 +1,27 @@
+#include <mpi.h>
+#include <fmt/printf.hpp>
+
+int main(int argc, char** argv) {
+   MPI_Init(&argc, &argv);
+
+   int rank; MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+   int nof_processes; MPI_Comm_size(MPI_COMM_WORLD, &nof_processes);
+
+   if (rank) {
+      MPI_Send(&rank, 1, MPI_INT, 0, 0, MPI_COMM_WORLD);
+   } else {
+      for (int i = 0; i + 1 < nof_processes; ++i) {
+	 MPI_Status status;
+	 int msg;
+	 MPI_Recv(&msg, 1, MPI_INT, MPI_ANY_SOURCE,
+	    0, MPI_COMM_WORLD, &status);
+	 int count;
+	 MPI_Get_count(&status, MPI_INT, &count);
+	 if (count == 1) {
+	    fmt::printf("%d\n", msg);
+	 }
+      }
+   }
+
+   MPI_Finalize();
+}
