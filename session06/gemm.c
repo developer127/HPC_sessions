30e5bb2e177fa1081a6ca781660c92044b713e62
Jonas Oberle <jonas.oberle@uni-ulm.de>
2017-03-22 14:04:34 +0100

    session6 fixed

--- session06/gemm.c
+++ session06/gemm.c
@@ -26,15 +26,15 @@
 #endif
 
 #ifndef MIN_M
-#define MIN_M   200
+#define MIN_M   100
 #endif
 
 #ifndef MIN_N
-#define MIN_N   200
+#define MIN_N   100
 #endif
 
 #ifndef MIN_K
-#define MIN_K   200
+#define MIN_K   100
 #endif
 
 #ifndef MAX_M
@@ -108,10 +108,35 @@ walltime()
    return ((double) times(&ts)) * ClockTick;
 }
 
-
+void
+printMatrix(size_t m, size_t n,
+            const double *A,
+            size_t incRowA, size_t incColA)
+{
+   for (size_t i = 0; i < m; ++i) {
+      printf("   ");
+      for (size_t j = 0; j < n; ++j) {
+         printf("%4.1lf ", A[i*incRowA+j*incColA]);
+      }
+      printf("\n");
+   }
+   printf("\n");
+}
 
 void
-initMatrix(size_t m, size_t n, double *A, ptrdiff_t incRowA, ptrdiff_t incColA)
+initMatrix(size_t m, size_t n,
+           double *A,
+           size_t incRowA, size_t incColA)
+{
+   for (size_t j = 0; j < n; ++j) {
+      for (size_t i = 0; i < m; ++i) {
+         A[i*incRowA+j*incColA] = (double)j*(double)m+(double)i+1;
+      }
+   }
+}
+
+void
+initRMatrix(size_t m, size_t n, double *A, ptrdiff_t incRowA, ptrdiff_t incColA)
 {
     for (size_t j=0; j<n; ++j) {
         for (size_t i=0; i<m; ++i) {
@@ -180,6 +205,45 @@ dgeaxpy(size_t m, size_t n,
     }
 }
 
+void
+dgescal(size_t m, size_t n,
+        double beta,
+        double *C, ptrdiff_t incRowC, ptrdiff_t incColC)
+{
+    // Compute:  C <- beta*C
+    if (beta!=1) {
+        if (beta!=0) {
+            if(incRowC>incColC){    // row major
+                for (size_t i=0; i<m; ++i) {
+                    for (size_t j=0; j<n; ++j) {
+                        C[i*incRowC+j*incColC] *=beta;
+                    }
+                }
+            } else {                // col major
+                for (size_t j=0; j<m; ++j) {
+                    for (size_t i=0; i<n; ++i) {
+                        C[i*incRowC+j*incColC] *=beta;
+                    }
+                }
+            }
+        } else {
+            if(incRowC>incColC){    // row major
+                for (size_t i=0; i<m; ++i) {
+                    for (size_t j=0; j<n; ++j) {
+                        C[i*incRowC+j*incColC] = 0;
+                    }
+                }
+            } else {                // col-major
+                for (size_t j=0; j<m; ++j) {
+                    for (size_t i=0; i<n; ++i) {
+                        C[i*incRowC+j*incColC] = 0;
+                    }
+                }
+            }
+        }
+    }
+}
+
 double
 asumMatrix(size_t m, size_t n,
            const double *A, ptrdiff_t incRowA, ptrdiff_t incColA)
@@ -238,14 +302,15 @@ estimateGemmResidual(size_t m, size_t n, size_t k,
 // GEMM simple block    schoolish version
 //------------------------------------------------------------------------------
 void
-gemm_block(const double *A, ptrdiff_t incRowA, ptrdiff_t incColA,
+gemm_block(size_t m, size_t n, size_t k,
+           const double *A, ptrdiff_t incRowA, ptrdiff_t incColA,
            const double *B, ptrdiff_t incRowB, ptrdiff_t incColB,
            double *C, ptrdiff_t incRowC, ptrdiff_t incColC)
 {
-    for (size_t i=0; i<M_C; ++i) {
-        for (size_t j=0; j<N_C; ++j) {
+    for (size_t i=0; i<m; ++i) {
+        for (size_t j=0; j<n; ++j) {
             double dot = 0;
-            for(size_t l=0; l<K_C; ++l) {
+            for(size_t l=0; l<k; ++l) {
                 dot += A[i*incRowA+l*incColA]
                     * B[l*incRowB+j*incColB];
             }
@@ -391,39 +456,8 @@ dgemm_var4(size_t m, size_t n, size_t k, double alpha,
            double *C, ptrdiff_t incRowC, ptrdiff_t incColC)
 {
     // Compute:  C <- beta*C
-    if (beta!=1.0) {
-        if (beta!=0) {
-            if(incRowC>incColC){    // row major
-                for (size_t i=0; i<m; ++i) {
-                    for (size_t j=0; j<n; ++j) {
-                        C[i*incRowC+j*incColC] *=beta;
-                    }
-                }
-            } else {                // col major
-                for (size_t j=0; j<m; ++j) {
-                    for (size_t i=0; i<n; ++i) {
-                        C[i*incRowC+j*incColC] *=beta;
-                    }
-                }
-
-            }
-        } else {
-            if(incRowC>incColC){    // row major
-                for (size_t i=0; i<m; ++i) {
-                    for (size_t j=0; j<n; ++j) {
-                        C[i*incRowC+j*incColC] = 0;
-                    }
-                }
-            } else {                // col-major
-                for (size_t j=0; j<m; ++j) {
-                    for (size_t i=0; i<n; ++i) {
-                        C[i*incRowC+j*incColC] = 0;
-                    }
-                }
-
-            }
-        }
-    }
+    dgescal(m, n, beta,
+            C, incRowC, incColC);
     // Compute: C <- C + alpha*A*B
     if(alpha == 0) {
         return;
@@ -434,7 +468,7 @@ dgemm_var4(size_t m, size_t n, size_t k, double alpha,
                 double temp_A = A[i*incRowA + l*incColA];
                 for(size_t j=0; j<n; ++j) {
                     C[i*incRowC+j*incColC] += alpha
-                                            * (temp_A * B[l*incRowB + j*incColB]);
+                                            * (temp_A*B[l*incRowB + j*incColB]);
                 }
             }
         }
@@ -463,108 +497,112 @@ dgemm_var5(size_t m, size_t n, size_t k, double alpha,
            double *C, ptrdiff_t incRowC, ptrdiff_t incColC)
 {
     // Compute:  C <- beta*C
-    if (beta!=1.0) {
-        if (beta!=0) {
-            if(incRowC>incColC){    // row major
-                for (size_t i=0; i<m; ++i) {
-                    for (size_t j=0; j<n; ++j) {
-                        C[i*incRowC+j*incColC] *=beta;
-                    }
-                }
-            } else {                // col major
-                for (size_t j=0; j<m; ++j) {
-                    for (size_t i=0; i<n; ++i) {
-                        C[i*incRowC+j*incColC] *=beta;
-                    }
-                }
-
-            }
-        } else {
-            if(incRowC>incColC){    // row major
-                for (size_t i=0; i<m; ++i) {
-                    for (size_t j=0; j<n; ++j) {
-                        C[i*incRowC+j*incColC] = 0;
-                    }
-                }
-            } else {                // col-major
-                for (size_t j=0; j<m; ++j) {
-                    for (size_t i=0; i<n; ++i) {
-                        C[i*incRowC+j*incColC] = 0;
-                    }
-                }
-
-            }
-        }
-    }
+    dgescal(m, n, beta,
+          C, incRowC, incColC);
     // Compute: C <- C + alpha*A*B
     if(alpha == 0.0) {
         return;
     }
+    // remainders
+    size_t mr = m%M_C;
+    size_t nr = n%N_C;
+    size_t kr = k%K_C;
+
     // Anzahl der in die Matritzen passenden Blöcke
-    size_t mb = m/M_C;
-    size_t nb = n/N_C;
-    size_t kb = k/K_C;
+    size_t mb = mr==0 ? m/M_C-1 : m/M_C;
+    size_t nb = nr==0 ? n/N_C-1 : n/N_C;
+    size_t kb = kr==0 ? k/K_C-1 : k/K_C;
 
-    for(size_t i=0; i<mb; ++i) {
-        for(size_t j=0; j<nb; ++j) {
+    for(size_t i=0; i<=mb; ++i) {
+        size_t M = (i<mb || mr==0) ? M_C : mr;
+        for(size_t j=0; j<=nb; ++j) {
+            size_t N = (j<nb || nr==0) ? N_C : nr;
             // Buffer C_ col_major
-            initZero(M_C,N_C,
-                     C_,1,M_C);
-            for(size_t l=0; l<kb; ++l) {
+            initZero(M, N,
+                     C_, 1, M);
+            for(size_t l=0; l<=kb; ++l) {
+                size_t K = (l<kb || kr==0) ? K_C : kr;
                 // Buffer A_ row_major
-                dgecopy(M_C,K_C,
+                dgecopy(M, K,
                         &A[i*incRowA*M_C+l*incColA*K_C], incRowA, incColA,
-                        A_,K_C,1);
+                        A_, K, 1);
                 // Buffer B_ col_major
-                dgecopy(K_C,N_C,
+                dgecopy(K, N,
                         &B[l*incRowB*K_C+j*incColB*N_C], incRowB, incColB,
-                        B_,1,K_C);
+                        B_,1,K);
 
-                gemm_block(A_,K_C,1,B_,1,K_C,C_,1,M_C);
+                gemm_block(M, N, K,
+                           A_, K, 1,
+                           B_, 1, K,
+                           C_, 1, M);
             }
-            dgeaxpy(M_C,N_C, alpha,
-                    C_,1,M_C,
+            dgeaxpy(M, N, alpha,
+                    C_, 1, M,
                     &C[i*incRowC*M_C+j*incColC*N_C], incRowC,incColC);
         }
-    }/*
-    // The cases when the blocks don't fit:
-    if(m%M_C != 0) {
-        dgemm_var4(m%M_C,nb*N_C,k,alpha,
-                  &A[mb*incRowA*M_C], incRowA, incColA,
-                  B,incRowB,incColB,
-                  1,
-                  &C[mb*incRowC*M_C], incRowC, incColC);
     }
-    if(n%N_C != 0) {
-        dgemm_var4(mb*M_C,nb*N_C,k%K_C,alpha,
-                  &A[kb*incColA*K_C], incRowA, incColA,
-                  &B[kb*incRowB*K_C], incRowB, incColB,
-                  1,
-                  C, incRowC, incColC);
-    }
-    if(m%M_C != 0 || n%N_C != 0){
-        dgemm_var4(mb*M_C,n%N_C,k,alpha,
-                  A, incRowA, incColA,
-                  &B[nb*incColB*N_C],incRowB,incColB,
-                  1,
-                  &C[nb*incColC*N_C], incRowC, incColC);
-    }*/
 }
 
 
 int
 main()
-{
-    initMatrix(MAXDIM_M, MAXDIM_K, A, 1, MAXDIM_M);
-    initMatrix(MAXDIM_K, MAXDIM_N, B, 1, MAXDIM_K);
-    initMatrix(MAXDIM_M, MAXDIM_N, C1, 1, MAXDIM_M);
+{/*
+    double A[4*4];
+    double B[4*4];
+    double C_1[4*4];
+    double C_2[4*4];
+
+    initRMatrix(4, 4, A, 1, 4);
+    initRMatrix(4, 4, B, 1, 4);
+    initRMatrix(4, 4, C_1, 1, 4);
+    dgecopy(4, 4, C_1, 1, 4, C_2, 1, 4);
+
+    printMatrix(4, 4, A, 1, 4);
+    printMatrix(4, 4, B, 1, 4);
+    printMatrix(4, 4, C_1, 1, 4);
+    printMatrix(4, 4, C_2, 1, 4);
+
+    dgemm_var4(4, 4, 4, 2,
+               A, 1, 4,
+               B, 1, 4,
+               1.5,
+               C_1, 1, 4);
+
+    printMatrix(4, 4, C_1, 1, 4);
+
+    //initMatrix(4, 4, C, 1, 4);
+    dgemm_var5(4, 4, 4, 2,
+               A, 1, 4,
+               B, 1, 4,
+               1.5,
+               C_2, 1, 4);
+
+    printMatrix(4, 4, C_2, 1, 4);
+    double dif;
+    dif = asumDiffMatrix(4, 4,
+                          C_1, 1, 4,
+                          C_2, 1, 4);
+    printf("Differenz: %lf\n", dif);
+
+    double res;
+    res = estimateGemmResidual(4, 4, 4, 2,
+                               A, 1, 4,
+                               B, 1, 4,
+                               0,
+                               C_1, 1, 4,
+                               C_2, 1, 4);
+    printf("Residuum: %lf\n", res);
+
+    */
+    initRMatrix(MAXDIM_M, MAXDIM_K, A, 1, MAXDIM_M);
+    initRMatrix(MAXDIM_K, MAXDIM_N, B, 1, MAXDIM_K);
+    initRMatrix(MAXDIM_M, MAXDIM_N, C1, 1, MAXDIM_M);
     dgecopy(MAXDIM_M, MAXDIM_N, C1, 1, MAXDIM_M, C2, 1, MAXDIM_M);
     dgecopy(MAXDIM_M, MAXDIM_N, C1, 1, MAXDIM_M, C3, 1, MAXDIM_M);
     dgecopy(MAXDIM_M, MAXDIM_N, C1, 1, MAXDIM_M, C4, 1, MAXDIM_M);
 
     // FIXME: Print a header for the output
     printf("%*c", 19, ' ');
-    printf("%16s ", "GEMM Variant 1");
     //printf("%*c", 2, ' ');
     printf("%16s ", "GEMM Variant 2");
     printf("%*c", 10, ' ');
@@ -574,8 +612,7 @@ main()
     printf("\n");
 
     printf("%5s %5s %5s ", "m", "n", "k");
-    printf("%7s %8s ", "t", "mflops");
-    printf("%7s %8s %10s", "t", "mflops", "res");
+    printf("%7s %8s", "t", "mflops");
     printf("%7s %8s %10s", "t", "mflops", "res");
     printf("%7s %8s %10s", "t", "mflops", "res");
     printf("\n");
@@ -597,18 +634,6 @@ main()
 
         printf("%5zu %5zu %5zu ", m, n, k);
 
-        // Bench GEMM variant 1
-        t = walltime();
-        dgemm_var1(m, n, k, ALPHA,
-                   A, incRowA, incColA,
-                   B, incRowB, incColB,
-                   BETA,
-                   C1, incRowC, incColC);
-        t = walltime() - t;
-        mflops = 2.*m/1000*n/1000*k/t;
-
-        printf("%7.2lf %8.2lf ", t, mflops);
-
         // Bench GEMM variant 2
         t = walltime();
         dgemm_var2(m, n, k, ALPHA,
@@ -618,14 +643,8 @@ main()
                    C2, incRowC, incColC);
         t = walltime() - t;
         mflops = 2.*m/1000*n/1000*k/t;
-        res = estimateGemmResidual(m, n, k, ALPHA,
-                                   A, incRowA, incColA,
-                                   B, incRowB, incColB,
-                                   BETA,
-                                   C1, incRowC, incColC,
-                                   C2, incRowC, incColC);
 
-        printf("%7.2lf %8.2lf %10.2e", t, mflops, res);
+        printf("%7.2lf %8.2lf", t, mflops);
 
         // Bench GEMM variant best choice
         t = walltime();
@@ -640,7 +659,7 @@ main()
                                    A, incRowA, incColA,
                                    B, incRowB, incColB,
                                    BETA,
-                                   C1, incRowC, incColC,
+                                   C2, incRowC, incColC,
                                    C3, incRowC, incColC);
 
         printf("%7.2lf %8.2lf %10.2e", t, mflops, res);
@@ -658,11 +677,10 @@ main()
                                    A, incRowA, incColA,
                                    B, incRowB, incColB,
                                    BETA,
-                                   C1, incRowC, incColC,
+                                   C2, incRowC, incColC,
                                    C4, incRowC, incColC);
 
         printf("%7.2lf %8.2lf %10.2e", t, mflops, res);
-
         printf("\n");
     }
 }
